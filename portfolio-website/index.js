b0cc1c12b60884e8530e88f29a8268bec408548b
Sakshi <sakshimaheshwari202@gmail.com>
2025-11-15 19:19:03 +0530

    initial commit

---
+++ portfolio-website/index.js
@@ -0,0 +1,47 @@
+const toggleBtn = document.getElementById("themeToggle");
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("light-theme");
+  const isLight = document.body.classList.contains("light-theme");
+  toggleBtn.textContent = isLight ? "🌞" : "🌙";
+});
+
+const typedTextSpan = document.querySelector(".typed-text");
+const textArray = [
+  "Software Engineer",
+  "UI/UX Designer",
+  "Web Developer",
+];
+
+const typingDelay = 50;
+const erasingDelay = 50;
+const newTextDelay = 200;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typedTextSpan.textContent = textArray[textArrayIndex].substring( 0,charIndex - 1 );
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  
+  if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
